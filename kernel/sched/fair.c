655f92336200bceda8f319dbbd6efb88dfc5ac3c
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2015-02-16 16:42:59 +0530

    sched: Fix bug in average nr_running and nr_iowait calculation
    sched_get_nr_running_avg() returns average nr_running and nr_iowait
    task count since it was last invoked. Fix several bugs in their
    calculation.
    
    * sched_update_nr_prod() needs to consider that nr_running count can
      change by more than 1 when CFS_BANDWIDTH feature is used
    
    * sched_get_nr_running_avg() needs to sum up nr_iowait count across
      all cpus, rather than just one
    
    * sched_get_nr_running_avg() could race with sched_update_nr_prod(),
      as a result of which it could use curr_time which is behind a cpu's
      'last_time' value. That would lead to erroneous calculation of
      average nr_running or nr_iowait.
    
    While at it, fix also a bug in BUG_ON() check in
    sched_update_nr_prod() function and remove unnecessary nr_running
    argument to sched_update_nr_prod() function.
    
    Change-Id: I46737614737292fae0d7204c4648fb9b862f65b2
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    
    Conflicts:
    
    	kernel/sched/fair.c
    
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1689,8 +1689,10 @@ static void throttle_cfs_rq(struct cfs_rq *cfs_rq)
 			dequeue = 0;
 	}
 
-	if (!se)
+	if (!se) {
+		sched_update_nr_prod(cpu_of(rq), task_delta, false);
 		rq->nr_running -= task_delta;
+	}
 
 	cfs_rq->throttled = 1;
 	cfs_rq->throttled_timestamp = rq->clock;
@@ -1737,8 +1739,10 @@ void unthrottle_cfs_rq(struct cfs_rq *cfs_rq)
 			break;
 	}
 
-	if (!se)
+	if (!se) {
+		sched_update_nr_prod(cpu_of(rq), task_delta, true);
 		rq->nr_running += task_delta;
+	}
 
 	/* determine whether we need to wake up potentially idle cpu */
 	if (rq->curr == rq->idle && rq->cfs.nr_running)
